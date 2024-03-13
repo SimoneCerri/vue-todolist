e1db2f2e8b679d503a143c6fcef0a714b00cac5f
SimoneCerri <iosonosimone@hotmail.it>
2024-03-13 17:23:08 +0100

    all bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,40 @@ createApp
     ({
         data() {
             return {
-                newItem: "",
-                toDoItems:[]
+                newItem: '',
+                toDoItems:[],
+                error: false,
             }
         },
         methods:
         {
             addItem()
             {
-                console.log(this.newItem);
+                console.log(this.newItem.lenght);
                 //console.log(this.toDoItems);
                 //this.toDoItems.text.unshift(this.newItem)
                 //console.log(toDoItems);
                 //console.log(this.toDoItems.entries);
-                let newObject = {
+                
+                if(this.newItem.lenght > 3)
+                {
+                    let newObject =
+                    {
                     text: this.newItem,
                     done:false
+                    }
+                    this.toDoItems.unshift(newObject)
+                    this.newItem = ""
+                    this.error = false;
                 }
-                this.toDoItems.push(newObject)
-                console.log(this.toDoItems);
-                                
+                else
+                {
+                    this.error = true
+                }
+                
+                
+                //console.log(this.toDoItems);
+
             }
         },
         mounted() {
